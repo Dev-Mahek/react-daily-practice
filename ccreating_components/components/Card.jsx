d65d10d62c522b91b15e5255b9f2f92ffd25eb0e
Mahek Roy <agentroykvsil@gmail.com>
2025-10-02 23:20:45 +0530

    Update Card.jsx

--- ccreating_components/components/Card.jsx
+++ ccreating_components/components/Card.jsx
@@ -6,3 +6,29 @@ const Card = (props) => {
 };
 
 export default Card;
+
+// ----------------Another Example-----------------
+
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="mr-7 bg-white text-black inline-block p-6 text-center rounded">
+      <img
+        className="ml-4 h-32 w-32 rounded-full mb-3"
+        src={props.photo}
+        alt=""
+      />
+      <h1 className="text-2xl font-semibold mb-4">{props.user}</h1>
+      <h4 className="text-blue-400">{props.prof}</h4>
+      <h2>
+        {props.city}, {props.age}
+      </h2>
+      <button className="mt-5 bg-emerald-600 text-white px-4 py-2 rounded font-medium">
+        Add Friend
+      </button>
+    </div>
+  );
+}
+
+export default Card;
