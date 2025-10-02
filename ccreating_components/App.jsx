e5d57e77ecb71aa6bcd61de1d1a0cd31433199b5
Mahek Roy <agentroykvsil@gmail.com>
2025-10-02 23:19:27 +0530

    Update App.jsx

--- ccreating_components/App.jsx
+++ ccreating_components/App.jsx
@@ -15,16 +15,82 @@ export default App;
 
 // ----------Passing Data in Components-----------
 
-// import React from "react";
-// import Card from "./components/card";
+import React from "react";
+import Card from "./components/card";
 
-// const App = () => {
-//   let user = "Mahek";
-//   return (
-//     <div>
-//       <Card user={user} />
-//     </div>
-//   );
-// };
+const App = () => {
+  let user = "Mahek";
+  return (
+    <div>
+      <Card user={user} />
+    </div>
+  );
+};
 
-// export default App;
+export default App;
+
+// ------------Another example of Passing Data in Components---------------
+
+import React from "react";
+import Card from "./components/card";
+
+function App() {
+  let users = [
+    {
+      name: "Aarav Sharma",
+      city: "Delhi",
+      age: 27,
+      profession: "Software Engineer",
+      profile_photo: "https://randomuser.me/api/portraits/men/11.jpg",
+    },
+    {
+      name: "Meera Nair",
+      city: "Bengaluru",
+      age: 24,
+      profession: "Graphic Designer",
+      profile_photo: "https://randomuser.me/api/portraits/women/21.jpg",
+    },
+    {
+      name: "Rohit Das",
+      city: "Kolkata",
+      age: 30,
+      profession: "Digital Marketer",
+      profile_photo: "https://randomuser.me/api/portraits/men/31.jpg",
+    },
+    {
+      name: "Ananya Gupta",
+      city: "Mumbai",
+      age: 26,
+      profession: "Content Writer",
+      profile_photo: "https://randomuser.me/api/portraits/women/41.jpg",
+    },
+    {
+      name: "Karan Verma",
+      city: "Chandigarh",
+      age: 29,
+      profession: "Entrepreneur",
+      profile_photo: "https://randomuser.me/api/portraits/men/51.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="p-10">
+        {users.map(function (elem, idx) {
+          return (
+            <Card
+              key={idx}
+              user={elem.name}
+              age={elem.age}
+              city={elem.city}
+              photo={elem.profile_photo}
+              prof={elem.profession}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
