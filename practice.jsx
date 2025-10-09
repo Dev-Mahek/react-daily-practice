5075c90ec0d69aa93fc7be28a1529f87167868ac
Mahek Roy <agentroykvsil@gmail.com>
2025-10-09 19:52:36 +0530

    Update practice.jsx

--- practice.jsx
+++ practice.jsx
@@ -34,4 +34,47 @@ export default App;
 
 //➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 
+import React, { useState } from "react";
 
+const App = () => {
+  // Declare a state variable 'num' with an initial value of 0
+  // 'setnum' is the function used to update the value of 'num'
+  const [num, setnum] = useState(0);
+
+  // Function to increase the number by 10 when called
+  const increase = () => {
+    setnum(num + 10);
+  };
+
+  // Function to decrease the number by 10 when called
+  const decrease = () => {
+    setnum(num - 10);
+  };
+
+  return (
+    <div className="m-5">
+      {/* Display the current number */}
+      <h1 className="text-5xl mb-5">Number is {num}</h1>
+
+      {/* Button to increase the number by 10 when clicked */}
+      <button
+        onClick={increase}
+        className="bg-green-400 hover:bg-green-500 py-1 px-2 rounded text-black"
+      >
+        Increment
+      </button>
+
+      {/* Button to decrease the number by 10 when clicked */}
+      <button
+        onClick={decrease}
+        className="bg-red-400 hover:bg-red-500 py-1 px-2 rounded text-black ml-2"
+      >
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default App;
+
+//➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
