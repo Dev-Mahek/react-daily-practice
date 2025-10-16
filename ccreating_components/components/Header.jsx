f45b631f79030941f783569b184f7e4d4564fb5a
Mahek Roy <dev-mahek@zohomail.in>
2025-10-16 18:31:20 +0530

    Update Header.jsx

--- ccreating_components/components/Header.jsx
+++ ccreating_components/components/Header.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Contact, House, ReceiptText, ShoppingBasket } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <nav className="bg-emerald-900 flex py-5 px-10 items-center justify-between">
-        <h2 className="text-4xl">Mahek</h2>
-        <div className="flex gap-8 items-center">
-          <h4 className="text-xl">About</h4>
-          <h4 className="text-xl">Contact</h4>
-          <h4 className="text-xl">Products</h4>
-          <h4 className="text-xl">Projects</h4>
+    <div className="relative h-screen w-full">
+      <nav
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+      bg-zinc-700 text-white p-3 rounded-2xl w-90 h-22"
+      >
+        <div className="flex gap-12 items-center justify-center mt-5 text-xl">
+          <Link to="/" className="font-semibold flex items-center gap-1">
+            <House />
+          </Link>
+          <Link to="/about" className="font-semibold flex items-center gap-1">
+            <ReceiptText />
+          </Link>
+          <Link to="/contact" className="font-semibold flex items-center gap-1">
+            <Contact />
+          </Link>
+          <Link to="/product" className="font-semibold flex items-center gap-1">
+            <ShoppingBasket />
+          </Link>
         </div>
       </nav>
     </div>
